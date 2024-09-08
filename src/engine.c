a0f945c76ffada78cc80280eddc289edde987c3e
Christian Schäl <c.schael@badgersystems.de>
2024-09-08 16:29:57 +0200

    Added Stencil Buffer to Window

--- src/engine.c
+++ src/engine.c
@@ -84,6 +84,7 @@ struct engine_s *engine_new(void) {
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
